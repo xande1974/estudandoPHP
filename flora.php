5b0480a7b426529f6f5a0ad66531829e530facb0
xande1974 <xandegalatico@hotmail.com>
2022-12-04 19:23:46 -0300

    mudei o titulo

--- flora.php
+++ flora.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Codigos PHP</title>
+    <title>ESTUDOS PHP</title>
 </head>
 
 <body>
@@ -13,11 +13,11 @@
     $nome = $_GET['nome'];
     $ano = $_GET['ano'];
     $sexo = $_GET['sexo'];
-    
+
     $idade = date("Y") - $ano;
     echo "Eu sou $nome sou $sexo e tenho $idade anos de idade";
     echo "<br>";
-   
+
     ?>
     <a href="fauna.php">voltar</a>
 
